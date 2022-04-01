a539b2b916095b555e55e04bf3adfe9627107102
cheujona853 <cheujona853@2260I-L42-74>
2022-04-01 11:04:14 -0400

    stuff

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.grpPlayerChoice = new System.Windows.Forms.GroupBox();
+            this.radRock = new System.Windows.Forms.RadioButton();
+            this.radPaper = new System.Windows.Forms.RadioButton();
+            this.radScissors = new System.Windows.Forms.RadioButton();
+            this.imgPlayer = new System.Windows.Forms.PictureBox();
+            this.imgOpponent = new System.Windows.Forms.PictureBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.grpPlayerChoice.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOpponent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "You";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F);
+            this.label2.Location = new System.Drawing.Point(12, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Opponent";
+            // 
+            // grpPlayerChoice
+            // 
+            this.grpPlayerChoice.Controls.Add(this.radScissors);
+            this.grpPlayerChoice.Controls.Add(this.radPaper);
+            this.grpPlayerChoice.Controls.Add(this.radRock);
+            this.grpPlayerChoice.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F);
+            this.grpPlayerChoice.Location = new System.Drawing.Point(472, 12);
+            this.grpPlayerChoice.Name = "grpPlayerChoice";
+            this.grpPlayerChoice.Size = new System.Drawing.Size(190, 152);
+            this.grpPlayerChoice.TabIndex = 7;
+            this.grpPlayerChoice.TabStop = false;
+            this.grpPlayerChoice.Text = "Players Choice";
+            // 
+            // radRock
+            // 
+            this.radRock.AutoSize = true;
+            this.radRock.Location = new System.Drawing.Point(7, 34);
+            this.radRock.Name = "radRock";
+            this.radRock.Size = new System.Drawing.Size(86, 33);
+            this.radRock.TabIndex = 0;
+            this.radRock.TabStop = true;
+            this.radRock.Text = "Rock";
+            this.radRock.UseVisualStyleBackColor = true;
+            this.radRock.CheckedChanged += new System.EventHandler(this.radRock_CheckedChanged);
+            // 
+            // radPaper
+            // 
+            this.radPaper.AutoSize = true;
+            this.radPaper.Location = new System.Drawing.Point(6, 73);
+            this.radPaper.Name = "radPaper";
+            this.radPaper.Size = new System.Drawing.Size(96, 33);
+            this.radPaper.TabIndex = 1;
+            this.radPaper.TabStop = true;
+            this.radPaper.Text = "Paper";
+            this.radPaper.UseVisualStyleBackColor = true;
+            this.radPaper.CheckedChanged += new System.EventHandler(this.radPaper_CheckedChanged);
+            // 
+            // radScissors
+            // 
+            this.radScissors.AutoSize = true;
+            this.radScissors.Location = new System.Drawing.Point(6, 112);
+            this.radScissors.Name = "radScissors";
+            this.radScissors.Size = new System.Drawing.Size(123, 33);
+            this.radScissors.TabIndex = 2;
+            this.radScissors.TabStop = true;
+            this.radScissors.Text = "Scissors";
+            this.radScissors.UseVisualStyleBackColor = true;
+            this.radScissors.CheckedChanged += new System.EventHandler(this.radScissors_CheckedChanged);
+            // 
+            // imgPlayer
+            // 
+            this.imgPlayer.Location = new System.Drawing.Point(12, 41);
+            this.imgPlayer.Name = "imgPlayer";
+            this.imgPlayer.Size = new System.Drawing.Size(435, 123);
+            this.imgPlayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgPlayer.TabIndex = 1;
+            this.imgPlayer.TabStop = false;
+            // 
+            // imgOpponent
+            // 
+            this.imgOpponent.Image = global::Part_6._5___PSR_Project.Properties.Resources.gigachad;
+            this.imgOpponent.Location = new System.Drawing.Point(12, 220);
+            this.imgOpponent.Name = "imgOpponent";
+            this.imgOpponent.Size = new System.Drawing.Size(435, 106);
+            this.imgOpponent.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgOpponent.TabIndex = 0;
+            this.imgOpponent.TabStop = false;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F);
+            this.btnSubmit.Location = new System.Drawing.Point(472, 170);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(190, 39);
+            this.btnSubmit.TabIndex = 8;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F);
+            this.lblResult.Location = new System.Drawing.Point(473, 220);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(87, 29);
+            this.lblResult.TabIndex = 9;
+            this.lblResult.Text = "Result:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.grpPlayerChoice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.imgPlayer);
+            this.Controls.Add(this.imgOpponent);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.grpPlayerChoice.ResumeLayout(false);
+            this.grpPlayerChoice.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgOpponent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox imgOpponent;
+        private System.Windows.Forms.PictureBox imgPlayer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox grpPlayerChoice;
+        private System.Windows.Forms.RadioButton radScissors;
+        private System.Windows.Forms.RadioButton radPaper;
+        private System.Windows.Forms.RadioButton radRock;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblResult;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,48 @@ namespace Part_6._5___PSR_Project
 {
     public partial class Form1 : Form
     {
+        Random generator = new Random();
         public Form1()
         {
             InitializeComponent();
+        }
+        private void radRock_CheckedChanged(object sender, EventArgs e)
+        {
+            imgPlayer.Image = Properties.Resources.ROCK;
+        }
+        private void radPaper_CheckedChanged(object sender, EventArgs e)
+        {
+            imgPlayer.Image = Properties.Resources.PAPERS;
+        }
+        private void radScissors_CheckedChanged(object sender, EventArgs e)
+        {
+            imgPlayer.Image = Properties.Resources.EdSciss;
+        }
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            int opponentResult = generator.Next(1,4);
+            string playerChoice = radRock;
+            
+
+            if (opponentResult == 1)
+            {
+                imgOpponent.Image = Properties.Resources.ROCK;
+            }
+            else if (opponentResult == 2)
+            {
+                imgOpponent.Image = Properties.Resources.PAPERS;
+            }
+            else if (opponentResult == 3)
+            {
+                imgOpponent.Image = Properties.Resources.EdSciss;
+            }
+
+
+            if (opponentResult == playerChoice)
+            {
+                lblResult.Text = "TIE";
+            }
+
         }
     }
 }
